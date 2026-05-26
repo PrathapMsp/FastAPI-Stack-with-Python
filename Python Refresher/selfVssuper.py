a2107d66e1c57585e61285f54fa73f9c9edd8e59
PrathapMsp <106061482+PrathapMsp@users.noreply.github.com>
2026-05-26 15:41:39 +0530

    adding python refresher files

--- Python Refresher/selfVssuper.py
+++ Python Refresher/selfVssuper.py
@@ -13,3 +13,12 @@ class Dog(Animal):
     def speak(self):
         super().speak()         # call parent method
         print(self.name, "barks")
+
+"""
+use: 
+d = Dog("Buddy", "Labrador")
+d.speak()
+o/p:
+Animal makes a sound
+Buddy barks
+"""
